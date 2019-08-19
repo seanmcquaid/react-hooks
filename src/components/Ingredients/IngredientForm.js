c9d4efabf7537e1aab1cac01bc9909e51f892492
seanmcquaid <sean.mcquaid2@verizon.net>
2019-08-18 22:52:25 -0400

    use context, use memo and creating custom hooks. Going to make my next react project strictly using hooks

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -7,6 +7,7 @@ import './IngredientForm.css';
 const IngredientForm = React.memo(props => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
+  console.log('RENDERING INGREDIENT FORM');
 
   const submitHandler = event => {
     event.preventDefault();
